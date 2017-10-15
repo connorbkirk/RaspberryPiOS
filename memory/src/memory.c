3a304c84e8c05330e64e73c70e76bba1dee91fb9
connorbkirk <ckirk96@gmail.com>
2017-10-15 19:45:12 -0400

    memory management @ kernel level

--- memory/src/memory.c
+++ memory/src/memory.c
@@ -36,16 +36,61 @@ void init_memory(){
 	first_block->state = LOCKED;
 }
 
+void increment_heap(size_t size){
+	//TODO
+}
+
 void *kmalloc(size_t size){
+	struct memory_block *current = first_block;
+	
+	bool finished = false;
+
+	do{
+		if(current->state == FREE && current->size >= size){
+			return (void *)((char *)current + sizeof(struct memory_block));
+		}
+		
+		if(current->next != NULL){
+			current = current->next;
+		}else if((char *)current - (size + sizeof(struct memory_block)) < (char *)heap_end){
+			increment_heap((size + sizeof(struct memory_block)) - ((uint32_t)current - (uint32_t)heap_end));
+			
+			struct memory_block *new_block = (struct memory_block*) heap_end;
+			new_block->next = NULL;
+			new_block->size = size;
+			new_block->state = LOCKED;
+
+			current->next = new_block;
+
+			return (void *)((char *)new_block + sizeof(struct memory_block));
+		}else{
+			finished = true;
+		}
+	}while(!finished);
 	return NULL;
 }
 
 void *krealloc(void *ptr, size_t size){
-	return NULL;
+	struct memory_block *block = (struct memory_block *)((char*)ptr - sizeof(struct memory_block));
+
+	if(block->size >= size){
+		return ptr;
+	}
+	
+	block->state = FREE;
+	
+	return kmalloc(size);
 }
 
 void *kcalloc(size_t size){
-	return NULL;
+	void *ptr = kmalloc(size);
+	uint8_t i;
+	
+	for(i = 0; i < size; i++){
+		*((uint8_t *)ptr + i) = 0;
+	}
+
+	return ptr;
 }
 
 void kfree(void *ptr){
