8d99feea0d1c0b83fc0ea2490e0f2eae05a03f61
connorbkirk <ckirk96@gmail.com>
2017-11-15 16:17:47 -0500

    updated irq

--- interrupts/src/interrupts.c
+++ interrupts/src/interrupts.c
@@ -1,5 +1,7 @@
 #include "interrupts.h"
 #include "gfx.h"
+#include "act.h"
+#include "time.h"
 
 void enable_timer_irq(){
 	IRQ->IRQ_enable_basic.Enable_Timer_IRQ = 1;
@@ -16,7 +18,14 @@ void __attribute__((interrupt("SWI"))) swi_handler(){
 }
 
 void __attribute__((interrupt("IRQ"))) irq_handler(){
+	static int lit = 0;
+
+	ARMTIMER->clear = 0;
+	
 	console_write("INTERRUPTED\n", 12);
+
+	lit = !lit;
+	set_act_state(lit);
 }
 
 void __attribute__((interrupt("FIQ"))) fiq_handler(){
