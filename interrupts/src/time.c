72abe472d6ca12737551ec6311af69e4b28ed057
Connor Kirk <ckirk96@gmail.com>
2017-12-13 19:02:32 -0500

    Update time.c

--- interrupts/src/time.c
+++ interrupts/src/time.c
@@ -4,8 +4,7 @@
 void wait(uint32_t time){
 	uint32_t end = SYSTEMTIMER->timer_lo + time;
 
-	while(SYSTEMTIMER->timer_lo < end){
-	}
+	while(SYSTEMTIMER->timer_lo < end);
 }
 
 void set_time_irq(uint32_t time){
